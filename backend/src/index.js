d507eed629cd5556ec4e9c2a5e7dc7b8b8e4c3de
Sayan-Rajak-Das <sayandas987123654@gmail.com>
2025-01-17 21:06:51 +0530

    test

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,6 +13,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true })); 
 app.use(cors());
 
+// Root route
+app.get('/', (req, res) => {
+    res.send('Welcome to the Social Media Handler API');
+});
+
 app.use("/app/auth", authRoutes)
 
 app.use("/app", userCrudRoutes)
