b1c7db87d1dbb3c532b58eee352036e0421b8ebb
Sayan Rajak Das <168341321+Sayan-Rajak-Das@users.noreply.github.com>
2025-01-22 02:24:44 -0800

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,7 +11,10 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); 
-app.use(cors());
+app.use(cors({
+    credentials : true,
+    origin : "*"
+}))
 
 // Root route
 app.get('/', (req, res) => {
